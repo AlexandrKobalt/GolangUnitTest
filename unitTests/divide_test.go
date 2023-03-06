a5d92372c34c25fcb77f639e61d8b7299107f7bc
AlexandrKobalt <72446601+AlexandrKobalt@users.noreply.github.com>
2023-03-06 14:01:43 +0300

    Added test table and logs

--- unitTests/divide_test.go
+++ unitTests/divide_test.go
@@ -7,15 +7,38 @@ import (
 
 func TestDivide(t *testing.T) {
 	// Arrange
-	var x float64 = 18.55
-	var y float64 = 5.3
-	var expected float64 = 3.5
+	testTable := []struct {
+		x        float64
+		y        float64
+		expected float64
+	}{
+		{
+			x:        18.55,
+			y:        5.3,
+			expected: 3.5,
+		},
+		{
+			x:        10,
+			y:        5,
+			expected: 2,
+		},
+		{
+			x:        158645.784356,
+			y:        578.6548954,
+			expected: 274.16303848312695014314053273971,
+		},
+	}
 
 	// Act
-	result := cmd.Divide(x, y)
+	for _, testCase := range testTable {
+		result := cmd.Divide(testCase.x, testCase.y)
 
-	// Assert
-	if !WithinTolerance(expected, result, 1e-12) {
-		t.Errorf("Incorrect result. Expected %f, got %f", expected, result)
+		// Run with "go test -v" to see logs
+		t.Logf("Calling Divide(%f, %f), result: %f\n", testCase.x, testCase.y, testCase.expected)
+
+		// Assert
+		if !WithinTolerance(testCase.expected, result, 1e-12) {
+			t.Errorf("Incorrect result. Expected %f, got %f", testCase.expected, result)
+		}
 	}
 }
